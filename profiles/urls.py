6a12c9ed13f3e271bab19fe4913b2f154194d51c
rubaiiyat <abirr1516@gmail.com>
2024-10-07 17:09:38 +0600

    Profile Update and change password done

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("auth/register/", views.userRegister.as_view(), name="register"),
     path("auth/login/", views.userLogin.as_view(), name="login"),
     path("profile", views.userProfile.as_view(), name="profile"),
+    path("profile/edit/", views.editUserProfile, name="edit"),
+    path("profile/changepassword/", views.changePass.as_view(), name="changepassword"),
     path("auth/logout/", views.userLogout, name="logout"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from .form import userForm
+from .form import userForm, editProfile
 from django.contrib import messages
 from django.views.generic import CreateView, TemplateView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.contrib.auth import logout
 
 
@@ -64,7 +64,46 @@ class userProfile(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["page"] = "profile"
+        context["user"] = self.request.user
         return context
+
+
+def editUserProfile(request):
+    page = "Edit Profile"
+
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = editProfile(request.POST, instance=request.user)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Profile Updated")
+                return redirect("profile")
+        else:
+            form = editProfile(instance=request.user)
+        return render(request, "editProfile.html", {"page": page, "form": form})
+    else:
+        return redirect("login")
+
+
+class changePass(PasswordChangeView):
+    template_name = "chngpass.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect("login")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "Change Password"
+        return context
+
+    def get_success_url(self) -> str:
+        return reverse_lazy("profile")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Password Changed")
+        return super().form_valid(form)
 
 
 def userLogout(request):
